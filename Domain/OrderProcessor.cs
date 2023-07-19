f2f89eaf972b321faf21b4b242fbc5a95f101f9c
gajalves <ga.jaimealves@gmail.com>
2023-07-19 10:57:11 -0400

    Order processor~

--- Domain/OrderProcessor.cs
+++ Domain/OrderProcessor.cs
@@ -4,7 +4,26 @@
     {
         public void Processor(Order? order)
         {
+            if(order != null)
+            {
+                if(order.IsVerified)
+                {
+                    if(order.Items.Count > 0)
+                    {
+                        if(order.Items.Count > 15)
+                        {
+                            throw new Exception("The order " + order.Id + " has too many items.");
+                        }
 
+                        if (order.OrderStatus != "ReadyToProcess")
+                        {
+                            throw new Exception("The order " + order.Id + " isn't ready to process");
+                        }
+
+                        order.IsProcessed = true;
+                    }
+                }
+            }
         }
     }
 }
