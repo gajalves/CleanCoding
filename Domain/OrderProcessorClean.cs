690d238d4fc8d500facc7918b80369f92b9c172d
gajalves <ga.jaimealves@gmail.com>
2023-07-19 11:50:38 -0400

    Early return e extraindo para um metodo com nome mais descritivo

---
+++ Domain/OrderProcessorClean.cs
@@ -0,0 +1,30 @@
+﻿namespace Domain
+{
+    public class OrderProcessorClean
+    {
+        public void Process(Order? order)
+        {
+            if (!IsOrderProcessable(order))            
+                return;
+            
+            if (order!.Items.Count > 15)
+            {
+                throw new Exception("The order " + order.Id + " has too many items.");
+            }
+
+            if (order.OrderStatus != "ReadyToProcess")
+            {
+                throw new Exception("The order " + order.Id + " isn't ready to process");
+            }
+
+            order.IsProcessed = true;
+        }
+
+        private static bool IsOrderProcessable(Order? order)
+        {
+            return order is null ||
+                   order.IsVerified ||
+                   order.Items.Any();
+        }
+    }
+}
